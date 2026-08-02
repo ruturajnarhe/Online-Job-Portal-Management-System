2d0576562a2b9d087abbe111538e90904a1f2fc7
ruturajnarhe <ruturajnarhe@gmail.com>
2026-08-03 01:01:49 +0530

    search by id

--- src/main/java/com/jsp/ojpms/controller/UserController.java
+++ src/main/java/com/jsp/ojpms/controller/UserController.java
@@ -3,6 +3,7 @@ package com.jsp.ojpms.controller;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -38,4 +39,9 @@ public class UserController {
 		return userService.getAllUsers();
 	}
 
+	@GetMapping("/{id}")
+	public User getUserById(@PathVariable int id) {
+		return userService.getUserById(id);
+	}
+	
 }
--- src/main/java/com/jsp/ojpms/repository/UserRepository.java
+++ src/main/java/com/jsp/ojpms/repository/UserRepository.java
@@ -4,6 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.jsp.ojpms.entity.User;
 
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Integer> {
 
 }
